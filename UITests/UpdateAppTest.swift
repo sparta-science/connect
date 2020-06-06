78334714debd7e76aa57ceb7509b8fbde29c9af3
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-06 11:08:29 -0700

    refactor: UI Tests to test upgrades from app cast
    remove mock server

---
+++ UITests/UpdateAppTest.swift
@@ -0,0 +1,39 @@
+import XCTest
+
+let kDefaultTimeout: TimeInterval = 5
+
+class UITests: XCTestCase {
+    let app = XCUIApplication()
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+        app.launch()
+    }
+
+    override func tearDownWithError() throws {
+        app.terminate()
+        try super.tearDownWithError()
+    }
+    
+    func testAutoUpgrade() throws {
+        let menuBarsQuery = app.menuBars
+        menuBarsQuery.menuBarItems["Help"].click()
+        menuBarsQuery.menuItems["Check for updates..."].click()
+        let updateDialog = app.dialogs["Software Update"]
+        updateDialog.waitToAppear()
+        updateDialog.staticTexts["Initial Release"].waitToAppear()
+        updateDialog.buttons["Install Update"].click()
+        
+        let updatingWindow = app.windows["Updating SpartaConnect"]
+        updatingWindow.staticTexts["Ready to Install"].waitToAppear()
+        updatingWindow.buttons["Install and Relaunch"].waitToAppear().click()
+        updatingWindow.waitToDisappear()
+        
+        app.windows["Window"].waitToAppear()
+        app.menuBars.menuBarItems["SpartaConnect"].click()
+        app.menuBars.menus.menuItems["About SpartaConnect"].click()
+        app.dialogs.staticTexts["Version 1.0 (1.0.3)"].waitToAppear()
+        app.dialogs.buttons[XCUIIdentifierCloseWindow].click()
+    }
+}
--- UITests/start-mock-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -l
-
-cd mockHttpServer
-python -m SimpleHTTPServer
