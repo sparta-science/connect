d275e9611ccc60aed0ec16edef9aaf4f9459cc4b
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-06 09:34:10 -0700

    - code-sign build on CI - add check for updates - enable automated updates download - lower current version to test updates - disable sandbox to allow updates to succeed - add app cast with update to version 1.0

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -1,26 +1,18 @@
-//
-//  AppDelegate.swift
-//  SpartaConnect
-//
-//  Created by Sparta Science on 6/2/20.
-//  Copyright © 2020 Sparta Science. All rights reserved.
-//
-
 import Cocoa
+import Sparkle
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    @IBAction func checkForUpdates(_ sender: Any) {
+        let updater = SUUpdater.shared()!
+        updater.checkForUpdates(sender)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let updater = SUUpdater.shared()!
+        assert(updater.automaticallyChecksForUpdates)
+        updater.automaticallyDownloadsUpdates = true
+        assert(updater.automaticallyDownloadsUpdates)
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
