1d6884730cb6eeb774a1a73038857313148d3927
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-06 19:28:04 -0700

    refactor: move updater to storyboard
    remove code from app delegate
    remove unused controller

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -1,17 +1,5 @@
 import Cocoa
-import Sparkle
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    let updater = SUUpdater.shared()!
-
-    @IBAction func checkForUpdates(_ sender: Any) {
-        updater.checkForUpdates(sender)
-    }
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        assert(updater.automaticallyChecksForUpdates)
-        updater.automaticallyDownloadsUpdates = true
-        assert(updater.automaticallyDownloadsUpdates)
-    }
 }
