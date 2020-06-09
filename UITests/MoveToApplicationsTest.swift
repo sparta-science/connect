5bd398fb132e5e20918864c6dd0b281ad3072e1a
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-08 19:50:03 -0700

    refactor: move to applications folder test
    to use tmp folder
    in order to leave built application intact
    so debugger is not getting confused

--- UITests/MoveToApplicationsTest.swift
+++ UITests/MoveToApplicationsTest.swift
@@ -2,53 +2,57 @@ import XCTest
 
 class MoveToApplicationsTest: XCTestCase {
     let movedUrl = URL(fileURLWithPath: "/Applications/SpartaConnect.app")
+    let tempUrl = URL(fileURLWithPath: "/tmp/SpartaConnect.app")
     let fileManager = FileManager.default
-    let app = XCUIApplication()
-    
-    func removeFromApplications() {
+    let builtApp = XCUIApplication()
+    lazy var tempApp = XCUIApplication(url: tempUrl)
+    lazy var movedApp = XCUIApplication(url: movedUrl)
+
+    func remove(url: URL) {
         try? fileManager.removeItem(at: movedUrl)
         XCTAssertFalse(fileManager.fileExists(atPath: movedUrl.path),
                        "should not be in Applications")
+    }
+    
+    func cleanup() {
+        [movedUrl, tempUrl].forEach { remove(url: $0) }
     }
 
     override func setUpWithError() throws {
         try super.setUpWithError()
         continueAfterFailure = false
-        removeFromApplications()
+        cleanup()
+        prepareTempApp()
     }
 
     override func tearDownWithError() throws {
-        removeFromApplications()
+        cleanup()
         try super.tearDownWithError()
     }
     
     func verifyRunningFromApplications() {
-        let restarted = XCUIApplication(url: movedUrl)
-        XCTAssertTrue(restarted.wait(for: .runningForeground, timeout: 5),
+        XCTAssertTrue(movedApp.wait(for: .runningForeground, timeout: 5),
                       "wait for app to restart from /Applications")
-        restarted.terminate()
+        movedApp.terminate()
     }
     
-    func launchAndChooseToMove() {
-        app.launchArguments = [
-            "-moveToApplicationsFolderAlertSuppress", "NO",
-        ]
-        app.launch()
-        let alert = app.dialogs["alert"]
+    func launchAndChooseToMoveToApplications() {
+        tempApp.launch()
+        let alert = tempApp.dialogs["alert"]
         alert.staticTexts["Move to Applications folder?"].waitToAppear()
         alert.buttons["Move to Applications Folder"].click()
+        alert.waitToDisappear()
+        XCTAssertTrue(tempApp.wait(for: .notRunning, timeout: 1), "wait for app to terminate")
     }
     
-    func restoreOriginalPath(path: String) {
-        try? fileManager.moveItem(at: movedUrl, to: URL(fileURLWithPath: path))
-        XCTAssertTrue(fileManager.fileExists(atPath: path))
+    func prepareTempApp() {
+        let pathBeforeMove = builtApp.value(forKeyPath: "_applicationImpl._path") as! String
+        try! fileManager.copyItem(at: URL(fileURLWithPath: pathBeforeMove), to: tempUrl)
+        XCTAssertTrue(fileManager.fileExists(atPath: tempUrl.path))
     }
 
     func testMoveToApplications() throws {
-        let pathBeforeMove = app.value(forKeyPath: "_applicationImpl._path") as! String
-        launchAndChooseToMove()
-        XCTAssertTrue(app.wait(for: .notRunning, timeout: 1), "wait for app to terminate")
+        launchAndChooseToMoveToApplications()
         verifyRunningFromApplications()
-        restoreOriginalPath(path: pathBeforeMove)
     }
 }
