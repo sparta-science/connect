742dac3b6a5e5f47d532cffbd0333219748ff0b8
Ben Summers <benlsummers@gmail.com>
2020-06-09 15:20:37 -0700

    rest of the changes
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -1,9 +1,25 @@
 import AppKit
 import LetsMove
+import NSBundle_LoginItem
 
 @NSApplicationMain
 public class AppDelegate: NSObject, NSApplicationDelegate {
     public func applicationDidFinishLaunching(_ notification: Notification) {
         PFMoveToApplicationsFolderIfNecessary()
+        updateLaunchAtLogin()
+    }
+    
+    @IBAction func toggleLaunchAtLogin(_ sender: NSMenuItem) {
+        if sender.state == .on {
+            Bundle.main.disableLoginItem()
+        } else {
+            Bundle.main.enableLoginItem()
+        }
+        updateLaunchAtLogin()
+    }
+    
+    func updateLaunchAtLogin() {
+        let launchAtLoginMenuItem = NSApp.mainMenu!.item(withTag: 3)
+        launchAtLoginMenuItem?.state = Bundle.main.isLoginItemEnabled() ? .on : .off
     }
 }
