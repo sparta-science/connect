5fc311ebcae24ea46eaa52457b2f361898dcfdb5
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-09 23:31:10 -0700

    increase wait and polling interval

--- AppSpecs/LoginItemManagerSpec.swift
+++ AppSpecs/LoginItemManagerSpec.swift
@@ -18,12 +18,12 @@ class LoginItemManagerSpec: QuickSpec {
             
             context("openAtLogin") {
                 it("should toggle login item") {
-                    [true, false].forEach {
+                    [false, true].forEach {
                         subject.openAtLogin = $0
                         expect(Bundle.main.isLoginItemEnabled())
                             .toEventually(equal($0),
-                                          timeout: 5,
-                                          pollInterval: 1)
+                                          timeout: 20,
+                                          pollInterval: 2)
                         expect(subject.openAtLogin) == $0
                     }
                 }
