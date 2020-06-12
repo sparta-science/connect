464303e2e5117614842211b30e63d64c4c40a228
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-12 12:21:20 -0700

    test: MenuBarButton

--- App/MenuBarButton.swift
+++ App/MenuBarButton.swift
@@ -1,14 +1,13 @@
 import AppKit
 
 public class MenuBarButton: NSObject {
-    var statusItem: NSStatusItem!
+    public var statusItem: NSStatusItem!
 
     public override func awakeFromNib() {
         super.awakeFromNib()
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         statusItem.menu = NSApp.mainMenu
         statusItem.autosaveName = Bundle.main.bundleIdentifier
-        statusItem.isVisible = true
         statusItem.behavior = [.terminationOnRemoval, .removalAllowed]
         statusItem.button?.image = #imageLiteral(resourceName: "menu-bar-icon")
         NSApp.mainMenu = nil
