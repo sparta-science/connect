9e9b5cc050587fdf3d80a76627f18bb3a3b0d950
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-13 21:29:57 -0700

    refactor: menu bar button to have own menu in storyboard

--- App/MenuBarButton.swift
+++ App/MenuBarButton.swift
@@ -2,14 +2,14 @@ import AppKit
 
 public class MenuBarButton: NSObject {
     public var statusItem: NSStatusItem!
-
+    @IBOutlet public weak var menu: NSMenu!
+    
     public override func awakeFromNib() {
         super.awakeFromNib()
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        statusItem.menu = NSApp.mainMenu
+        statusItem.menu = menu
         statusItem.autosaveName = Bundle.main.bundleIdentifier
         statusItem.behavior = [.terminationOnRemoval, .removalAllowed]
         statusItem.button?.image = #imageLiteral(resourceName: "menu-bar-icon")
-        NSApp.mainMenu = nil
     }
 }
--- AppSpecs/MenuBarButtonSpec.swift
+++ AppSpecs/MenuBarButtonSpec.swift
@@ -6,31 +6,28 @@ class MenuBarButtonSpec: QuickSpec {
     override func spec() {
         describe("MenuBarButton") {
             var subject: MenuBarButton!
+            var statusMenu: NSMenu!
             beforeEach {
                 subject = .init()
+                statusMenu = .init(title: "test menu")
+                subject.menu = statusMenu
             }
             context("awakeFromNib") {
-                var mainMenu: NSMenu!
-                beforeEach {
-                    mainMenu = .init(title: "test menu")
-                    NSApp.mainMenu = mainMenu
-                }
                 context("statusItem") {
                     var statusItem: NSStatusItem!
                     beforeEach {
                         subject.awakeFromNib()
                         statusItem = subject.statusItem
                     }
-                    it("should have take main menu and clear it") {
-                        expect(statusItem.menu) === mainMenu
-                        expect(NSApp.mainMenu).to(beNil())
+                    it("should have take main menu from subject") {
+                        expect(statusItem.menu) === statusMenu
                     }
                     it("should have correct properties") {
                         expect(statusItem.button?.image) == #imageLiteral(resourceName: "menu-bar-icon")
                         expect(statusItem.isVisible) == true
                         expect(statusItem.behavior) == [.terminationOnRemoval, .removalAllowed]
                         expect(statusItem.autosaveName) == Bundle.main.bundleIdentifier
-                        expect(statusItem.length) == -2.0
+                        expect(statusItem.length) == NSStatusItem.squareLength
                     }
                 }
             }
