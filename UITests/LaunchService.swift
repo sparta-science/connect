2f649797af3b46446ba63b3d71591909cd7190c5
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-14 22:27:07 -0700

    fix: app is not ready for launch
    by increasing timeout
    
    https://github.com/sparta-science/connect/pull/14/checks?check_run_id=771384721

--- UITests/LaunchService.swift
+++ UITests/LaunchService.swift
@@ -14,7 +14,8 @@ enum LaunchService {
     static func waitForAppToBeReadyForLaunch(at url: URL) {
         let ready = XCTNSPredicateExpectation(predicate: isReadyToBeLaunched(),
                                               object: url)
-        XCTWaiter.wait(until: ready, "app is not ready for launch at: \(url)")
+        XCTWaiter.wait(until: ready, timeout: .install,
+                       "app is not ready for launch at: \(url)")
         let values = try! url.resourceValues(forKeys: [.quarantinePropertiesKey])
         XCTAssertNil(values.quarantineProperties, "should have no quarantine properties")
     }
