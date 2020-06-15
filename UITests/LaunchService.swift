073bb2de8b46e76dc2eb3fe731886e53eabaf321
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-15 01:05:33 -0700

    refactor: launch service to work on slow machine also

--- UITests/LaunchService.swift
+++ UITests/LaunchService.swift
@@ -2,16 +2,20 @@ import XCTest
 
 enum LaunchService {
     private static func isReadyToBeLaunched() -> NSPredicate {
-        NSPredicate { object, _  in
+        let workspace = NSWorkspace.shared
+        return NSPredicate { object, _  in
             if let appUrl = object as? URL,
-                let url = LSCopyDefaultApplicationURLForURL(appUrl as CFURL, .all, nil) {
-                return (url.takeRetainedValue() as URL) == appUrl
+                let url = workspace.urlForApplication(toOpen: appUrl) {
+                return url == appUrl
             }
             return false
         }
     }
 
     static func waitForAppToBeReadyForLaunch(at url: URL) {
+        let workspace = NSWorkspace.shared
+        let fileType = try! workspace.type(ofFile: url.path)
+        XCTAssertEqual(kUTTypeApplicationBundle as String, fileType)
         let ready = XCTNSPredicateExpectation(predicate: isReadyToBeLaunched(),
                                               object: url)
         XCTWaiter.wait(for: ready)
