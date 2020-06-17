681b17d732ea0429f12f0ab899354c2221fcf5c7
Ben Gottlieb <ben@standalone.com>
2020-06-17 16:40:12 -0500

    added new mini app

---
+++ ConnectCredentials/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ConnectCredentials
+//
+//  Created by Ben Gottlieb on 6/17/20.
+//  Copyright © 2020 Sparta Science. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
