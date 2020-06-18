d28332870c58e7d0805192de0c9bd9b05a7951c3
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-17 17:07:02 -0700

    add deflake test script
    Co-authored-by: Ben Summers <benlsummers@gmail.com>

---
+++ bin/dev/deflake-ui-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+rm -rf test-results
+set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme SpartaConnect clean build | xcpretty
+time while { set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme UITests test -resultBundlePath test-results/ui-tests-$(date +%s) | xcpretty } do;done
