a774ddb3ac082b80fab30b0c2a08f94ed4c11326
Ben Summers <benlsummers@gmail.com>
2020-06-17 17:12:04 -0700

    kill app before running tests in script
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- bin/dev/deflake-ui-tests.sh
+++ bin/dev/deflake-ui-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 rm -rf test-results
+killall SpartaConnect
 set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme SpartaConnect clean build | xcpretty
 time while { set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme UITests test -resultBundlePath test-results/ui-tests-$(date +%s) | xcpretty } do;done
