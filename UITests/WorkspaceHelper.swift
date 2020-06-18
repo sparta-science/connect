c5081de4189bd43cfe87c4e641e1885736fb13ae
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-17 18:59:20 -0700

    specify architecture for launching the app

--- UITests/WorkspaceHelper.swift
+++ UITests/WorkspaceHelper.swift
@@ -14,6 +14,7 @@ class WorkspaceHelper {
         config.allowsRunningApplicationSubstitution = true
         config.promptsUserIfNeeded = false
         config.addsToRecentItems = false
+        config.architecture = CPU_TYPE_X86_64
         return config
     }
     
