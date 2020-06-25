73b7903ece27aa67b825c80e105b013b56076669
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-25 11:50:51 -0700

    add observer on selected tab

--- App/TabViewController.swift
+++ App/TabViewController.swift
@@ -1,4 +1,11 @@
 import Cocoa
 
 class TabViewController: NSTabViewController {
+    var observation: NSKeyValueObservation?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        observation = observe(\.selectedTabViewItemIndex, options: [.new, .old]) { controller, change in
+            print("transition from \(change.oldValue!) to \(change.newValue!)")
+        }
+    }
 }
