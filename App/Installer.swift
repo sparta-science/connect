2d1d57969a02078c5aa4944d22a84b5aaf42888f
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-25 12:04:32 -0700

    add notification publisher

--- App/Installer.swift
+++ App/Installer.swift
@@ -1,6 +1,13 @@
 import Foundation
 
-struct Installer {
+class Installer: NSObject {
+    var center: NotificationCenter = .default
+    let name: Notification.Name = .init("user operation")
+
+    override func awakeFromNib() {
+//        let subscriber =
+//        center.publisher(for: name).receive(subscriber: Subscriber)
+    }
     func beginInstallation(login: Login) {
         
     }
--- App/TabViewController.swift
+++ App/TabViewController.swift
@@ -1,11 +1,17 @@
 import Cocoa
 
 class TabViewController: NSTabViewController {
-    var observation: NSKeyValueObservation?
+    var observation: NSKeyValueObservation!
+    var center: NotificationCenter = .default
+    let name: Notification.Name = .init("user operation")
+    var notePublisher: NotificationCenter.Publisher!
     override func viewDidLoad() {
         super.viewDidLoad()
+        notePublisher = center.publisher(for: name)
+        
         observation = observe(\.selectedTabViewItemIndex, options: [.new, .old]) { controller, change in
             print("transition from \(change.oldValue!) to \(change.newValue!)")
+            
         }
     }
 }
