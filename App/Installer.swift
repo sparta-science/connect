05cabfd031f0c52fd14fe608794139c59d34823d
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-27 16:48:27 -0700

    cleanup: mock download

--- App/Installer.swift
+++ App/Installer.swift
@@ -84,25 +84,6 @@ public struct HTTPLoginMessage: Codable {
 public class Installer: NSObject {
     public static let shared = Installer()
     @Published public var state: State = .login
-    @objc func downloadStep() {
-        if case let .busy(value: value) = state {
-            if value.isFinished {
-                state = .complete
-            } else {
-                value.completedUnitCount += 1
-                state = .busy(value: value)
-                perform(#selector(downloadStep), with: nil, afterDelay: 0.1)
-            }
-        }
-    }
-    @objc func downloadStart() {
-        let progress = Progress()
-        progress.isCancellable = true
-        progress.totalUnitCount = 20
-        progress.completedUnitCount = 1
-        self.state = .busy(value: progress)
-        perform(#selector(downloadStep), with: nil, afterDelay: 1)
-    }
     var cancellables = Set<AnyCancellable>()
     
     enum ApiError: LocalizedError {
