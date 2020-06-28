60473e009ba0fefaadebb4bf43e270ca93054582
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-27 17:15:38 -0700

    refactor: creating download

--- App/Installer.swift
+++ App/Installer.swift
@@ -133,18 +133,30 @@ public class Installer: NSObject {
         state = .busy(value: progress)
     }
     
-    func createDownload(url: URL) -> AnyPublisher<URL, Error> {
+    func downloadDestination() -> (destinationURL: URL, options: DownloadRequest.Options) {
+        (
+            destinationURL: self.downloadUrl(),
+            options: [.createIntermediateDirectories, .removePreviousFile]
+        )
+    }
+    
+    func destination(_ temporaryURL: URL, _ response: HTTPURLResponse)
+        -> (destinationURL: URL, options: DownloadRequest.Options) {
+            print("download status: \(response.statusCode)")
+            print("download temp file: \(temporaryURL.path)")
+            return downloadDestination()
+    }
+    
+    func futureDownload(url: URL) -> Future<URL?, AFError> {
         Future<URL?, AFError> { promise in
-            AF.download(url) { (tempUrl: URL, response: HTTPURLResponse) in
-                        print("download status: \(response.statusCode)")
-                        print("download temp file: \(tempUrl.path)")
-                return (destinationURL: self.downloadUrl(),
-                                options: [.createIntermediateDirectories, .removePreviousFile])
-                    }
-                    .response { response in
-                        promise(response.result)
-            }.downloadProgress(closure: self.downloading(_:))
+            AF.download(url, to: self.destination(_:_:))
+                .response { promise($0.result) }
+                .downloadProgress(closure: self.downloading(_:))
         }
+    }
+    
+    func createDownload(url: URL) -> AnyPublisher<URL, Error> {
+        futureDownload(url: url)
         .compactMap {$0}
         .mapError {$0}
         .eraseToAnyPublisher()
