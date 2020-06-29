c4b2258d743b301d67c1d2f2b8cf0608426a9819
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-29 11:25:55 -0700

    add progress and connected scenes UI layout
    add outlets and actions

--- App/ConnectedController.swift
+++ App/ConnectedController.swift
@@ -1,5 +1,7 @@
 import Cocoa
 
 class ConnectedController: NSViewController {
+    @IBAction func disconnect(_ sender: NSButton) {
+    }
 }
 
--- App/ProgressController.swift
+++ App/ProgressController.swift
@@ -1,4 +1,10 @@
 import Cocoa
 
 class ProgressController: NSViewController {
+    @IBOutlet public var progressIndicator: NSProgressIndicator!
+    @IBOutlet public var cancelButton: NSButton!
+    @IBOutlet public var progressLabel: NSTextField!
+    @IBAction func cancelInstallation(_ sender: NSButton) {
+    }
+
 }
