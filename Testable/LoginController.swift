948deb4acd0ec65e4c41655231bb49a3073b7369
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-30 23:40:12 -0700

    remove: representedObject in favor of direct access to login

--- Testable/LoginController.swift
+++ Testable/LoginController.swift
@@ -2,14 +2,10 @@ import Cocoa
 
 public class LoginController: NSViewController {
     @objc var hideEnvironments: Bool = isReleaseBuild()
-    public let login = Login()
+    @objc public let login = Login()
     @Inject var installer: Installation
 
     @IBAction public func connectAction(_ sender: NSButton) {
         installer.beginInstallation(login: login)
-    }
-    override public func awakeFromNib() {
-        super.awakeFromNib()
-        representedObject = login
     }
 }
