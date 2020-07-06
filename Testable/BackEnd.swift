43e176da282b7cdec93ef2b6709ae38d7f875066
Abraham Choe <abraham.choe@gmail.com>
2020-07-06 11:42:25 -0700

    add fake server to the backends
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- Testable/BackEnd.swift
+++ Testable/BackEnd.swift
@@ -3,16 +3,19 @@ import Foundation
 public enum BackEnd: String, CaseIterable {
     // swiftlint:disable explicit_enum_raw_value
     case localhost
+    case fakeServer
     case staging
     case production
     public func choices() -> [String] {
         AllCases().map { $0.rawValue }
     }
     public func baseUrl() -> URL {
+        let fileUrlString = Bundle.main.url(forResource: "successful-response", withExtension: "json")!.absoluteString
         let environment: [BackEnd: String] = [
-            .localhost: "http://localhost:4000",
-            .staging: "https://staging.spartascience.com",
-            .production: "https://home.spartascience.com"
+            .localhost: "http://localhost:4000/api/app-setup",
+            .fakeServer: fileUrlString,
+            .staging: "https://staging.spartascience.com/api/app-setup",
+            .production: "https://home.spartascience.com/api/app-setup"
         ]
         return URL(string: environment[self]!)!
     }
