292c734bbe5f4b0cdaf46bb789551a8978987ed5
Paul Zabelin <paulz@users.noreply.github.com>
2020-07-05 20:31:46 -0700

    refactor: login request to use Init

--- Testable/LoginRequest.swift
+++ Testable/LoginRequest.swift
@@ -2,12 +2,14 @@ import Foundation
 
 public func loginRequest(_ login: Login) -> URLRequest {
     let backend = BackEnd(rawValue: login.environment)!
-    let loginUrl = backend.baseUrl().appendingPathComponent("api/app-setup")
-    var components = URLComponents(url: loginUrl, resolvingAgainstBaseURL: true)!
-    components.queryItems = [.init(name: "email", value: login.username),
-                             .init(name: "password", value: login.password),
-                             .init(name: "client-id", value: "delete-me-please-test")]
-    var request = URLRequest(url: components.url!)
-    request.httpMethod = "POST"
-    return request
+    let loginUrl = backend.baseUrl()
+    let components = Init(URLComponents(string: loginUrl.absoluteString)!) {
+        $0.path = "/api/app-setup"
+        $0.queryItems = [.init(name: "email", value: login.username),
+                         .init(name: "password", value: login.password),
+                         .init(name: "client-id", value: "delete-me-please-test")]
+    }
+    return Init(URLRequest(url: components.url!)) {
+        $0.httpMethod = "POST"
+    }
 }
