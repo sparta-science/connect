2686ee4de17fe82ade6c5e76b5e4b049af0fd881
Paul Zabelin <paulz@users.noreply.github.com>
2020-07-07 12:36:09 -0700

    add alamofire pod
    Co-authored-by: Abraham Choe <abraham.choe@gmail.com>

--- Pods/Target Support Files/Pods-SpartaConnect/Pods-SpartaConnect-frameworks.sh
+++ Pods/Target Support Files/Pods-SpartaConnect/Pods-SpartaConnect-frameworks.sh
@@ -197,18 +197,18 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LetsMove/LetsMove.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSBundle+LoginItem/NSBundle_LoginItem.framework"
   install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
-  install_dsym "${PODS_ROOT}/Sparkle/Sparkle.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwinjectAutoregistration/SwinjectAutoregistration.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LetsMove/LetsMove.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSBundle+LoginItem/NSBundle_LoginItem.framework"
   install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
-  install_dsym "${PODS_ROOT}/Sparkle/Sparkle.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwinjectAutoregistration/SwinjectAutoregistration.framework"
 fi
