ff7beaef5f40d27a47398d49d1828e13f0733a5e
Paul Zabelin <paulz@users.noreply.github.com>
2020-07-07 19:37:12 -0700

    test: download error

--- AppSpecs/DownloaderSpec.swift
+++ AppSpecs/DownloaderSpec.swift
@@ -20,7 +20,7 @@ class DownloaderSpec: QuickSpec {
                     let url = URL(string: "https://raw.githubusercontent.com/sparta-science/connect/master/LICENSE")!
                     waitUntil(timeout: 30.0) { done in
                         subject.createDownload(url: url) { progress in
-                            expect(progress.totalUnitCount) == -1
+                            expect([1_074, -1]).to(contain(progress.totalUnitCount))
                             expect(progress.completedUnitCount) == 1_074
                         }.sink(receiveCompletion: { completion in
                             if case .finished = completion {
@@ -29,6 +29,24 @@ class DownloaderSpec: QuickSpec {
                         }) { downloadedUrl in
                             let contents = try! String(contentsOf: downloadedUrl)
                             expect(contents).to(contain("Copyright (c) 2020 sparta-developers"))
+                        }.store(in: &cancellables)
+                    }
+                }
+                // TODO: pz - should report error instead of downloading error page
+                it("should report errors") {
+                    var cancellables = Set<AnyCancellable>()
+                    let url = URL(string: "https://raw.githubusercontent.com/foo")!
+                    waitUntil(timeout: 30.0) { done in
+                        subject.createDownload(url: url) { progress in
+                            expect(progress.totalUnitCount) == 20
+                            expect(progress.completedUnitCount) == 20
+                        }.sink(receiveCompletion: { completion in
+                            if case .finished = completion {
+                                done()
+                            }
+                        }) { downloadedUrl in
+                            let contents = try! String(contentsOf: downloadedUrl)
+                            expect(contents) == "400: Invalid request"
                         }.store(in: &cancellables)
                     }
                 }
