febf01bc38b1c04b0089985e58084d28af667dfc
Paul Zabelin <paulz@users.noreply.github.com>
2020-07-07 18:08:01 -0700

    redibility

--- App/Downloader.swift
+++ App/Downloader.swift
@@ -3,17 +3,19 @@ import Combine
 import Testable
 
 public class Downloader: Downloading {
-    public func createDownload(url: URL, reporting: @escaping (Progress) -> Void) -> AnyPublisher<URL, Error> {
-        futureDownload(url: url, reporting: reporting)
-        .compactMap { $0 }
-        .mapError { $0 }
-        .eraseToAnyPublisher()
+    public func createDownload(url: URL,
+                               reporting: @escaping (Progress) -> Void)
+        -> AnyPublisher<URL, Error> {
+            futureDownload(url: url, reporting: reporting)
+                .compactMap { $0 }
+                .mapError { $0 }
+                .eraseToAnyPublisher()
     }
 
     private func futureDownload(url: URL,
                                 reporting: @escaping (Progress) -> Void)
         -> Future<URL?, AFError> {
-            Future<URL?, AFError> { promise in
+            .init() { promise in
                 AF.download(url)
                     .response { promise($0.result) }
                     .downloadProgress(closure: reporting)
