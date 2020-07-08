acc57b310d62c07706658f0c8886bd2c5621e9d8
Paul Zabelin <paulz@users.noreply.github.com>
2020-07-07 18:50:33 -0700

    test: downloader by downloading small file

--- AppSpecs/DownloaderSpec.swift
+++ AppSpecs/DownloaderSpec.swift
@@ -14,17 +14,23 @@ class DownloaderSpec: QuickSpec {
                 }
             }
             context(Downloader.createDownload(url:reporting:)) {
-                it("should download a file") {
+                it("should download a text file reporting size") {
                     var cancellables = Set<AnyCancellable>()
-                    let url = testBundleUrl("expected_vernal_falls.tar.gz")
-                    subject.createDownload(url: url) { progress in
-                        print(progress)
-                    }.sink(receiveCompletion: { completion in
-                        print(completion)
-                    }) { downloadedUrl in
-                        print(downloadedUrl)
-                    }.store(in: &cancellables)
-                    RunLoop.run(for: 500)
+
+                    let url = URL(string: "https://raw.githubusercontent.com/sparta-science/connect/master/LICENSE")!
+                    waitUntil(timeout: 30.0) { done in
+                        subject.createDownload(url: url) { progress in
+                            expect(progress.totalUnitCount) == -1
+                            expect(progress.completedUnitCount) == 1_074
+                        }.sink(receiveCompletion: { completion in
+                            if case .finished = completion {
+                                done()
+                            }
+                        }) { downloadedUrl in
+                            let contents = try! String(contentsOf: downloadedUrl)
+                            expect(contents).to(contain("Copyright (c) 2020 sparta-developers"))
+                        }.store(in: &cancellables)
+                    }
                 }
             }
         }
