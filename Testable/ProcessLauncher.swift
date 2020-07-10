0ffa965b0370bbb37c5b00ca14c8a3576eccaad9
Ben Summers <benlsummers@gmail.com>
2020-07-10 15:55:31 -0700

    add process launcher
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

---
+++ Testable/ProcessLauncher.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public class ProcessLauncher {
+    public init() {}
+    public func runShellScript(script: URL) throws {
+
+    }
+}
---
+++ UnitSpecs/ProcessLauncherSpec.swift
@@ -0,0 +1,19 @@
+import Nimble
+import Quick
+import Testable
+
+class ProcessLauncherSpec: QuickSpec {
+    override func spec() {
+        describe(ProcessLauncher.self) {
+            var subject: ProcessLauncher!
+            beforeEach {
+                subject = .init()
+            }
+            describe(ProcessLauncher.runShellScript(script:)) {
+                xit("should report error when failing to launch") {
+                    try! subject.runShellScript(script: testBundleUrl("expected-config.yml"))
+                }
+            }
+        }
+    }
+}
