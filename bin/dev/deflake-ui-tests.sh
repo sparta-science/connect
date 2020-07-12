152816e4344080dafb5f1f5998815f850ecb9162
Paul Zabelin <paulz@users.noreply.github.com>
2020-07-12 15:11:04 -0700

    submit metrics while deflaking

--- bin/dev/deflake-ui-tests.sh
+++ bin/dev/deflake-ui-tests.sh
@@ -5,5 +5,7 @@ rm /tmp/stop-ui-tests.txt
 killall SpartaConnect
 set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme SpartaConnect clean build | xcpretty
 time while { set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme UITests test -resultBundlePath test-results/ui-tests-$(date +%s) | xcpretty } do
-    test -f /tmp/stop-ui-tests.txt && exit 5
+    source $(dirname $0)/../post-sparta-metrics.sh
+#    Stop tests when signaled by test observer
+#    test -f /tmp/stop-ui-tests.txt && exit 5
 done
