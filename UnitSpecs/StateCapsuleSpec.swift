bb1d61ff01fc19cca60c5f217ea693220470dc7e
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-04 15:26:34 -0700

    test: start receiving
    Co-authored-by: Ben Summers <benlsummers@gmail.com>

--- UnitSpecs/StateCapsuleSpec.swift
+++ UnitSpecs/StateCapsuleSpec.swift
@@ -5,15 +5,26 @@ import Testable
 class StateCapsuleSpec: QuickSpec {
     override func spec() {
         describe(StateCapsule.self) {
+            var defaults: UserDefaults!
+            var subject: StateCapsule!
+            beforeEach {
+                defaults = .init()
+                TestDependency.register(Inject(defaults!))
+                subject = .init()
+            }
             context(StateCapsule.init) {
-                var defaults: UserDefaults!
                 beforeEach {
-                    defaults = .init()
                     defaults.set(true, forKey: "complete")
-                    TestDependency.register(Inject(defaults!))
                 }
                 it("should load initial state") {
                     expect(StateCapsule().state) == .complete
+                }
+            }
+
+            context(StateCapsule.startReceiving) {
+                it("should start state progress") {
+                    subject.startReceiving()
+                    expect(subject.state.progress()).notTo(beNil())
                 }
             }
         }
