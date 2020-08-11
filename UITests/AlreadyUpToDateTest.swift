d9b8fe2de6df5744831a1df35ed0c0a45d4da3c1
Kelly Felkins <kelly@restlater.com>
2020-08-11 11:56:13 -0700

    adds test for already up-to-date alert
    Co-authored-by: Ben Summers <benlsummers@gmail.com>
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

---
+++ UITests/AlreadyUpToDateTest.swift
@@ -0,0 +1,38 @@
+import XCTest
+
+class AlreadyUpToDateTest: XCTestCase {
+    let tempAppHelper = TempAppHelper()
+    lazy var app = SpartaConnectApp()
+    let arguments = ["-moveToApplicationsFolderAlertSuppress", "YES"]
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        app.terminate()
+        try super.tearDownWithError()
+    }
+
+    func testAlreadyUpToDate() {
+        app.launchArguments = arguments
+        app.launch()
+        checkForUpdates()
+        verifyUpToDate()
+    }
+
+    func checkForUpdates() {
+        app.clickStatusItem()
+        app.statusBarMenu().menuItems["Check for updates..."].click()
+    }
+
+    func verifyUpToDate() {
+        let updateAlert = app.dialogs["alert"]
+        updateAlert.waitToAppear()
+        print(app.debugDescription)
+
+        updateAlert.staticTexts["You’re up-to-date!"].waitToAppear()
+        updateAlert.buttons["OK"].click()
+    }
+}
