09064c03d84eb9efd3d289cbfd34dfdc17901fe9
Kelly Felkins <kelly@restlater.com>
2020-08-11 14:50:31 -0700

    refactor test to use regex
    Co-authored-by: Ben Summers <benlsummers@gmail.com>
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- UnitSpecs/GetMacSerialNumberSpec.swift
+++ UnitSpecs/GetMacSerialNumberSpec.swift
@@ -5,18 +5,8 @@ import Testable
 class GetMacSerialNumberSpec: QuickSpec {
     override func spec() {
         describe(getMacSerialNumber) {
-            var subject: String!
-            beforeEach {
-                subject = getMacSerialNumber()
-            }
-            it("returns a string of 12 characters") {
-                expect(subject).to(haveCount(12))
-            }
-            it("the 'subject' should be alphanumeric characters") {
-                expect(subject.rangeOfCharacter(from: CharacterSet.alphanumerics.inverted)).to(beNil())
-            }
-            it("should be upper case") {
-                expect(subject.uppercased()) == subject
+            it("should be a 12 character string of uppercase alphanumerics") {
+                expect(getMacSerialNumber()).to(match("[A-Z0-9]{12}"))
             }
         }
     }
