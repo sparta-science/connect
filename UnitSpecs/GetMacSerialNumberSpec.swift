05b694d871a1278a19388ec4b5232d5fef252057
Kelly Felkins <kelly@restlater.com>
2020-08-11 14:44:59 -0700

    validates the serial number is uppercase alphanumeric
    Co-authored-by: Ben Summers <benlsummers@gmail.com>
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- UnitSpecs/GetMacSerialNumberSpec.swift
+++ UnitSpecs/GetMacSerialNumberSpec.swift
@@ -5,8 +5,18 @@ import Testable
 class GetMacSerialNumberSpec: QuickSpec {
     override func spec() {
         describe(getMacSerialNumber) {
+            var subject: String!
+            beforeEach {
+                subject = getMacSerialNumber()
+            }
             it("returns a string of 12 characters") {
-                expect(getMacSerialNumber()).to(haveCount(12))
+                expect(subject).to(haveCount(12))
+            }
+            it("the 'subject' should be alphanumeric characters") {
+                expect(subject.rangeOfCharacter(from: CharacterSet.alphanumerics.inverted)).to(beNil())
+            }
+            it("should be upper case") {
+                expect(subject.uppercased()) == subject
             }
         }
     }
