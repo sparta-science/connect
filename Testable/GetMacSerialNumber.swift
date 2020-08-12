c3726c3b627560ddb7d99e4abf68d8a6f3b41ac8
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-12 01:04:07 -0700

    fix: memory leak when serial number is invalid
    refactor for readability

--- Testable/GetMacSerialNumber.swift
+++ Testable/GetMacSerialNumber.swift
@@ -2,29 +2,36 @@ import Foundation
 
 /**
  https://ourcodeworld.com/articles/read/1113/how-to-retrieve-the-serial-number-of-a-mac-with-swift
+ https://developer.apple.com/library/archive/technotes/tn1103/
  Retrieves the serial number of your mac device.
  
  - Returns: The string with the serial.
  */
 public func getMacSerialNumber() -> String {
     var serialNumber: String? {
-        let platformExpert = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice") )
+        let platformExpert = IOServiceGetMatchingService(
+            kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice")
+        )
 
         guard platformExpert > 0 else {
             return nil
         }
 
-        guard let serialNumber = (IORegistryEntryCreateCFProperty(platformExpert,
-                                                                  kIOPlatformSerialNumberKey as CFString,
-                                                                  kCFAllocatorDefault,
-                                                                  0).takeUnretainedValue() as? String)?
-            .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) else {
-                return nil
+        defer {
+            IOObjectRelease(platformExpert)
         }
 
-        IOObjectRelease(platformExpert)
+        let serialNumberKey = kIOPlatformSerialNumberKey as CFString
+        let property = IORegistryEntryCreateCFProperty(platformExpert,
+                                                       serialNumberKey,
+                                                       kCFAllocatorDefault,
+                                                       0)
 
-        return serialNumber
+        guard let serialNumber = property?.takeUnretainedValue() as? String else {
+            return nil
+        }
+
+        return serialNumber.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
 
     return serialNumber ?? "Unknown"
