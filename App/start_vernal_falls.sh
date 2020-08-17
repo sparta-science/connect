9d1d5a78ceacbfe7027cd79ccb737cee4341baeb
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-17 15:25:07 -0700

    add shell commands

--- App/start_vernal_falls.sh
+++ App/start_vernal_falls.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 /bin/launchctl bootstrap gui/$(id -u) sparta_science.vernal_falls.plist
+result=$?
+
+if [ $result -eq 37 ]
+then
+    exit 0
+else
+    exit $result
+fi
--- App/stop_vernal_falls.sh
+++ App/stop_vernal_falls.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-/bin/launchctl bootout gui/$(id -u)/sparta_science.vernal_falls
+stop_command="/bin/launchctl bootout gui/$(id -u)/sparta_science.vernal_falls"
+echo $stop_command
 
+while [ eval ($stop_command -eq 37) ]
+do
+    sleep 1
+done
