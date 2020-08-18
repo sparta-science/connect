59422bc9fdd83f3fe2286b9f39ad8f06deefaee8
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-17 23:04:18 -0700

    add constants for BSD system error codes

--- App/start_vernal_falls.sh
+++ App/start_vernal_falls.sh
@@ -3,7 +3,12 @@
 /bin/launchctl bootstrap gui/$(id -u) sparta_science.vernal_falls.plist
 result=$?
 
-if (( result == 37 ))
+# BSD system error codes used by launchctl
+# e.g. launchctl error 37
+# https://github.com/apple/darwin-xnu/blob/master/bsd/sys/errno.h
+EALREADY=37 # Operation already in progress.
+
+if (( result == EALREADY ))
 then
     exit 0
 else
--- App/stop_vernal_falls.sh
+++ App/stop_vernal_falls.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+# BSD system error codes used by launchctl
+# e.g. launchctl error 36
+# https://github.com/apple/darwin-xnu/blob/master/bsd/sys/errno.h
+EINPROGRESS=36 # Operation now in progress
+ESRCH=3        # No such process
+
 while true
 do
     /bin/launchctl bootout gui/$(id -u)/sparta_science.vernal_falls
     result=$?
-    if (( result == 36 ))
+    if (( result == EINPROGRESS ))
     then
         sleep 1
     else
-        if (( result == 3 ))
+        if (( result == ESRCH ))
         then
             exit 0
         else
