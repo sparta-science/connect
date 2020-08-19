d11fb476ad2fa48ad83762a27f1aececec90bb44
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-19 02:02:31 -0700

    fix: https://github.com/sparta-science/connect/issues/47
    by ignoring Bootstrap failed: 5: Input/output error on Big Sur only
    
    Co-authored-by: Ben Summers <benlsummers@gmail.com>

--- App/start_vernal_falls.sh
+++ App/start_vernal_falls.sh
@@ -6,9 +6,21 @@ result=$?
 # BSD system error codes used by launchctl
 # e.g. launchctl error 37
 # https://github.com/apple/darwin-xnu/blob/master/bsd/sys/errno.h
+EIO=5       # Input/output error
 EALREADY=37 # Operation already in progress.
 
-if (( result == EALREADY ))
+# On macOS Big Sur 11.0 Beta (20A5343i):
+# subsequent launches of the launchctl service cause Input/output error.
+# https://github.com/sparta-science/connect/issues/47#issuecomment-675864268
+if [[ $(sw_vers -productVersion) == 11.* ]]
+then
+    IGNORE_ERROR=$EIO
+else
+    IGNORE_ERROR=$EALREADY
+fi
+
+
+if (( result == IGNORE_ERROR ))
 then
     exit 0
 else
