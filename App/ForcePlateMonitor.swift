58d6b6b84a17e8a63328a1565ecff0ece8215750
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-19 18:49:49 -0700

    [#174235432] filter USB devices

--- App/ForcePlateMonitor.swift
+++ App/ForcePlateMonitor.swift
@@ -5,14 +5,22 @@ protocol Starting {
     func start()
 }
 
+public enum Identifier: Int {
+    case stMicroelectronics = 0x0483
+    case virtualComPort = 0x5740
+}
+
 public class ForcePlateMonitor {
     let serialDeviceMonitor: SerialDeviceMonitor
     public init(monitor: SerialDeviceMonitor) {
         serialDeviceMonitor = monitor
     }
     public func start() {
-        serialDeviceMonitor.filterDevices = { _ in
-            []
+        serialDeviceMonitor.filterDevices = {
+            $0.filter {
+                $0.vendorId == Identifier.stMicroelectronics.rawValue
+                    && $0.productId == Identifier.virtualComPort.rawValue
+            }
         }
         DispatchQueue.global(qos: .background).async {
             self.serialDeviceMonitor.start()
--- AppSpecs/ForcePlateMonitorSpec.swift
+++ AppSpecs/ForcePlateMonitorSpec.swift
@@ -1,21 +1,20 @@
 import Nimble
 import Quick
 import SpartaConnect
-import USBDeviceSwift
+@testable import USBDeviceSwift
 
 class FakeMonitor: SerialDeviceMonitor {
     var didStartOnBackground: Bool?
     override func start() {
         didStartOnBackground = !Thread.isMainThread
     }
-    var filter: (([SerialDevice]) -> [SerialDevice])?
-    override var filterDevices: (([SerialDevice]) -> [SerialDevice])? {
-        get {
-            filter
-        }
-        set {
-            filter = newValue
-        }
+}
+
+extension SerialDevice {
+    init(vendor: Identifier, product: Identifier) {
+        self.init(path: "")
+        vendorId = vendor.rawValue
+        productId = product.rawValue
     }
 }
 
@@ -36,6 +35,25 @@ class ForcePlateMonitorSpec: QuickSpec {
                 it("should configure filter") {
                     subject.start()
                     expect(fake.filterDevices).notTo(beNil())
+                }
+                context(\SerialDeviceMonitor.filterDevices) {
+                    var devices: [SerialDevice]!
+                    beforeEach {
+                        let valid = SerialDevice(vendor: .stMicroelectronics, product: .virtualComPort)
+                        let noVendor = SerialDevice(path: "")
+                        let wrongProduct = SerialDevice(vendor: .stMicroelectronics, product: .stMicroelectronics)
+                        let wrongVendor = SerialDevice(vendor: .virtualComPort, product: .virtualComPort)
+                        devices = [
+                            valid,
+                            noVendor,
+                            wrongProduct,
+                            wrongVendor
+                        ]
+                        subject.start()
+                    }
+                    it("should only allow valid") {
+                        expect(fake.filterDevices!(devices)).to(haveCount(1))
+                    }
                 }
             }
         }
