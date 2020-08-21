92aae2aef502d63f1cf37149b840bcb877d6eb5a
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-21 15:29:36 -0700

    [#174455636] add support for FTDI USB UART

--- App/ForcePlateMonitor.swift
+++ App/ForcePlateMonitor.swift
@@ -4,6 +4,8 @@ import USBDeviceSwift
 public enum Identifier: Int {
     case stMicroelectronics = 0x0483
     case virtualComPort = 0x5740
+    case ftdi = 0x0403
+    case ftdiUsbUart = 0x6001
 }
 
 public class ForcePlateMonitor {
@@ -20,8 +22,11 @@ extension ForcePlateMonitor: ForcePlateDetection {
     public func start(updating: @escaping (String?) -> Void) {
         serialDeviceMonitor.filterDevices = {
             $0.filter {
-                $0.vendorId == Identifier.stMicroelectronics.rawValue
-                    && $0.productId == Identifier.virtualComPort.rawValue
+                ($0.vendorId == Identifier.stMicroelectronics.rawValue
+                    && $0.productId == Identifier.virtualComPort.rawValue)
+                ||
+                ($0.vendorId == Identifier.ftdi.rawValue
+                    && $0.productId == Identifier.ftdiUsbUart.rawValue)
             }
         }
         let add = center.addObserver(forName: .SerialDeviceAdded, object: nil, queue: .main) { note in
