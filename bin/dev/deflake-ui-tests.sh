8f25a371f34bf971cb024d09f8f3e77ff4b57767
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-20 22:24:07 -0700

    run ui tests repeatedly on CI to detect flaky tests (#62)
    * add deflake workflow
    * refactor: deflake script to invoke xc-scheme
    * add time limit to run test repeatedly

--- bin/dev/deflake-ui-tests.sh
+++ bin/dev/deflake-ui-tests.sh
@@ -2,6 +2,10 @@
 
 # Run UITest repetedly until first failure
 # optional argument: events - run until until an event is detected
+# Environment:
+#   TIME_LIMIT_MINUTES - limit time in minutes to stop running tests
+
+SECONDS=0
 
 die () {
     echo >&2 "$@"
@@ -10,12 +14,21 @@ die () {
 
 test $# -eq 0 || test "$1" = "events" || die "optional argument: events"
 
+xc_scheme=$(dirname $0)/../xc-scheme.sh
+
+xcode_scheme() {
+    source $xc_scheme "$@"
+}
+
 rm -rf test-results
 rm /tmp/events-detected-during-ui-tests.txt
 killall SpartaConnect
-set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme SpartaConnect clean build | xcpretty
-time while { set -o pipefail && xcodebuild -workspace SpartaConnect.xcworkspace -scheme UITests test -resultBundlePath test-results/ui-tests-$(date +%s) | xcpretty } do
+xcode_scheme SpartaConnect clean build
+time while { xcode_scheme UITests test -resultBundlePath test-results/ui-tests-latest } do
     source $(dirname $0)/../post-sparta-metrics.sh
+    mv test-results/ui-tests-latest.xcresult test-results/ui-tests-$(date +%s).xcresult
+    rm test-results/ui-tests-latest
     test "$1" = "events" && test -f /tmp/events-detected-during-ui-tests.txt && die "stopping because of events"
+    (( SECONDS / 60 > TIME_LIMIT_MINUTES )) && echo "reached time limit $TIME_LIMIT_MINUTES minutes" && exit 0
 done
 die "completed deflaking"
