4b33318303989e535e0a817c88797ef76e7e380e
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-25 13:56:21 -0700

    add staging server for debug build
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- Testable/DebugServerLocator.swift
+++ Testable/DebugServerLocator.swift
@@ -1,16 +1,27 @@
+public enum DebugApiServer: String, CaseIterable {
+    // swiftlint:disable:next explicit_enum_raw_value
+    case staging
+    func server() -> ApiServer {
+        .staging
+    }
+}
+
 public class DebugServerLocator: ServerLocator {
     @Inject var bundle: Bundle
 
     override public var availableServers: [String] {
         super.availableServers
+            + DebugApiServer.allCases.map { $0.rawValue }
             + DebugFileServer.allCases.map { $0.rawValue }
     }
 
     override func baseUrlString(_ server: String) -> String {
-        guard let file = DebugFileServer(rawValue: server) else {
-            return super.baseUrlString(server)
+        if let file = DebugFileServer(rawValue: server) {
+            return bundle.url(forResource: file.fileName(),
+                              withExtension: "json")!.absoluteString
+        } else if let debugServer = DebugApiServer(rawValue: server) {
+            return debugServer.server().serverUrlString()
         }
-        return bundle.url(forResource: file.fileName(),
-                          withExtension: "json")!.absoluteString
+        return super.baseUrlString(server)
     }
 }
