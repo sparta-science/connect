ef24185e01b4417e88fef0aebaf0522740eeff50
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-25 14:04:21 -0700

    update production environment name

--- Testable/Login.swift
+++ Testable/Login.swift
@@ -2,7 +2,7 @@ import Foundation
 
 @objcMembers
 public class Login: NSObject {
-    public var environment: String = "production"
+    public var environment: String = "home.spartascience.com"
     public var username: String?
     public var password: String?
 }
