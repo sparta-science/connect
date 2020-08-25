210f216514fbf3c44a0bccae60470f74561976ef
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-25 14:16:53 -0700

    select login environment depending on the defaults
    Co-authored-by: Ben Summers <benlsummers@gmail.com>

--- Testable/Login.swift
+++ Testable/Login.swift
@@ -6,4 +6,6 @@ public class Login: NSObject {
     public lazy var environment: String = defaults.bool(forKey: "offline installation") ? "offline" : "production"
     public var username: String?
     public var password: String?
+    public lazy var environment: String =
+        defaults.bool(forKey: "offline installation") ? "Sparta Offline System" : "home.spartascience.com"
 }
