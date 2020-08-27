b6f6476854fd1b6bd82b3b5c2dd81a8adf86ad56
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-26 17:14:09 -0700

    approve mocked science
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- App/Science.swift
+++ App/Science.swift
@@ -2,6 +2,6 @@ import Testable
 
 public func science(inputs: ScienceInputs) -> ScienceOutputs {
     ScienceOutputs(instances: inputs.input.map {
-        Instance(id: $0.id, features: Features(mskHealth: 0.424_371_634, approved: false))
+        Instance(id: $0.id, features: Features(mskHealth: 0.424_371_634, approved: true))
     })
 }
