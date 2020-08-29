0b6bcdb36234303aac82407769f2f17aee445a9d
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-29 00:56:31 -0700

    set cancel to true

--- bin/github/check-self-hosted-runner.swift
+++ bin/github/check-self-hosted-runner.swift
@@ -8,7 +8,7 @@ func checkIfScreenIsLocked() {
         if let locked = session["CGSSessionScreenIsLocked"] {
             print("screen locked: ", locked)
             print("::warning ::Not ready to run as screen is locked")
-            print("::set-output name=cancel::screen is locked")
+            print("::set-output name=cancel::true")
         }
     }
 }
