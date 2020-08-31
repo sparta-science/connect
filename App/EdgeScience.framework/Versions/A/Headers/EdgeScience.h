f14e9ac7be49fdc0e68275c673fc646ec236af8c
Ben Summers <benlsummers@gmail.com>
2020-08-31 14:15:42 -0700

    add edge science from initial release v1.0
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

---
+++ App/EdgeScience.framework/Versions/A/Headers/EdgeScience.h
@@ -0,0 +1,19 @@
+//
+//  EdgeScience.h
+//  EdgeScience
+//
+//  Created by Sparta Science on 8/28/20.
+//  Copyright © 2020 Sparta Science. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EdgeScience.
+FOUNDATION_EXPORT double EdgeScienceVersionNumber;
+
+//! Project version string for EdgeScience.
+FOUNDATION_EXPORT const unsigned char EdgeScienceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EdgeScience/PublicHeader.h>
+
+
