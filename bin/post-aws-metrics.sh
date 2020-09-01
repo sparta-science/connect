77cf398f3a80a7bcd59cfee81f489960b567a7be
Ben Summers <benlsummers@gmail.com>
2020-08-31 17:05:57 -0700

    use $TMPDIR for temporary files
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- bin/post-aws-metrics.sh
+++ bin/post-aws-metrics.sh
@@ -1,1 +1,1 @@
-aws cloudwatch put-metric-data --namespace "Rare Events" --metric-data file:///tmp/aws-ui-test-metrics.json
+aws cloudwatch put-metric-data --namespace "Rare Events" --metric-data file://$TMPDIR/aws-ui-test-metrics.json
--- bin/post-sparta-metrics.sh
+++ bin/post-sparta-metrics.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash -o errexit
 
 curl --output /dev/null --silent --fail --location --request POST $SUBMIT_METRICS_URL \
--d @"/tmp/sparta-ui-test-metrics-url-encoded-form.txt"
+-d @"$TMPDIR/sparta-ui-test-metrics-url-encoded-form.txt"
