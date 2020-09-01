000c582d7175c6ccbb29d7bc0fa182a40e7ef5c0
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-31 21:12:06 -0700

    refactor: notarize to use env vars

--- bin/dev/notarize.sh
+++ bin/dev/notarize.sh
@@ -2,15 +2,25 @@
 
 # Notarize app and wait for the response
 
-xcodebuild -exportArchive -archivePath build/archive.xcarchive \
-    -exportOptionsPlist App/export-options.plist \
+# Required Environment variables
+#
+# DEVELOPER_ID_LOGIN
+# DEVELOPER_ID_PASSWORD
+#
+# ARCHIVE=build/archive.xcarchive
+# EXPORT_OPTIONS=App/export-options.plist
+# APP_NAME=SpartaConnect
+# BUNDLE_ID=com.spartascience.SpartaConnect
+
+xcodebuild -exportArchive -archivePath "$ARCHIVE" \
+    -exportOptionsPlist "$EXPORT_OPTIONS" \
     -exportPath build/export
-ditto -c -k --keepParent build/export/SpartaConnect.app build/export/SpartaConnect-Unnotarized.zip
+ditto -c -k --keepParent "build/export/$APP_NAME.app" build/export/To-Be-Notarized.zip
 xcrun altool --notarize-app \
-    --primary-bundle-id "com.spartascience.SpartaConnect" \
+    --primary-bundle-id "$BUNDLE_ID" \
     --username $DEVELOPER_ID_LOGIN \
     --password @env:DEVELOPER_ID_PASSWORD \
-    --file build/export/SpartaConnect-Unnotarized.zip \
+    --file build/export/To-Be-Notarized.zip \
     --output-format xml > build/notarizing-request.plist
 request=$(/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID"  build/notarizing-request.plist)
 echo Notarization Request ID: $request
@@ -25,5 +35,5 @@ while [[ "$request_status" == "in progress" ]]; do
     request_status=$(/usr/libexec/PlistBuddy -c "Print :notarization-info:Status"  build/notarization-request-status.plist)
     echo $request_status
 done
-xcrun stapler staple build/export/SpartaConnect.app
-ditto -c -k --keepParent build/export/SpartaConnect.app build/export/SpartaConnect.zip
+xcrun stapler staple "build/export/$APP_NAME.app"
+ditto -c -k --keepParent "build/export/$APP_NAME.app" "build/export/$APP_NAME.zip"
