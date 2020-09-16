f0aa36e4d08151606010cb5e12fd33aaeae64bc0
Paul Zabelin <paulz@users.noreply.github.com>
2020-09-16 09:59:01 -0700

    refactor inputs
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- Testable/ScienceInputs.swift
+++ Testable/ScienceInputs.swift
@@ -2,11 +2,12 @@ import Foundation
 
 public struct ScienceInputs: Codable {
     public let input: [Input]
+
+    public struct Input: Codable {
+        public let load, explode, drive, avgMass: Double
+        public let maxVerticalJumpHeight: Double
+        public let gender: Int
+        public let id: String
+    }
 }
 
-public struct Input: Codable {
-    public let load, explode, drive, avgMass: Double
-    public let maxVerticalJumpHeight: Double
-    public let gender: Int
-    public let id: String
-}
