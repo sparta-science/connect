9e33789571b0283784381b4daa51488c6b78cb2c
Ben Summers <benlsummers@gmail.com>
2020-09-21 16:40:53 -0700

    Refactor UserDefaults to use a unique suite per test
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- UnitSpecs/UserDefaults+CreateAndInject.swift
+++ UnitSpecs/UserDefaults+CreateAndInject.swift
@@ -1,5 +1,14 @@
 import Foundation
+import Testable
+@testable import Quick
 
 extension UserDefaults: CreateAndInject {
     typealias ActAs = UserDefaults
+    static func createAndInject() -> Self {
+        let exampleName = World.sharedWorld.currentExampleMetadata!.example.name
+        let uniqueSuite = "test-user-defaults-" + exampleName
+        return Init(UserDefaults(suiteName: uniqueSuite)!) {
+            TestDependency.register(Inject<UserDefaults>($0))
+            } as! Self
+    }
 }
